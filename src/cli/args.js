03954e3fc0c5c751f93c33a6aeddb9797927e6f9
Elem <elem15@yandex.ru>
2022-06-01 19:34:47 +0300

    feat: solve src/sli tasks from assignment.md

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
+const env = process.argv.slice(2);
+
 export const parseArgs = () => {
-    // Write your code here 
+    for (let i = 0; i < env.length; i++) {
+        const arg = env[i];
+        const prefix = arg.slice(2);
+        if (env[i + 1]) {
+            const expression = `${prefix} is ${env[i + 1]}, `
+            process.stdout.write(expression);
+            i++;
+        }
+    }
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
+const env = process.argv.slice(2);
+
 export const parseEnv = () => {
-    // Write your code here 
+    for (let i = 0; i < env.length; i++) {
+        const arg = env[i];
+        const prefix = arg.slice(0, 4);
+        if (prefix === 'RSS_') {
+            if (env[i + 1]) {
+                const expression = `${arg}=${env[i + 1]}; `
+                process.stdout.write(expression);
+                i++;
+            }
+        }
+    }
 };
+
+parseEnv();
