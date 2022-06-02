b11e518ac39bf33afa4fc86d5c1a057790fe0bdc
Elem <elem15@yandex.ru>
2022-06-02 13:32:50 +0300

    feat: solve stream tasks from assignment.md

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from 'process';
+
+const invite = '\n\nEnter a new text to reverse:\n'
+
+const myTransform = new Transform({
+    writableObjectMode: true,
+    transform(chunk, _, callback) {
+        const data = chunk.toString().split('').reverse().join('');
+        callback(null, data + invite);
+    },
+});
+
 export const transform = async () => {
-    // Write your code here 
+    stdout.write(invite);
+    stdin.pipe(myTransform).pipe(stdout);
 };
+
+transform();
