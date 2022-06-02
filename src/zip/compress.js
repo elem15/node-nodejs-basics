d3ed74d0f40834fd2117aaf4c1f642cfcbb1a9c8
Elem <elem15@yandex.ru>
2022-06-02 16:00:16 +0300

    feat: solve zip tasks from assignment.md

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToSrc = path.join(__dirname, 'files', 'fileToCompress.txt');
+const pathToDestination = path.join(__dirname, 'files', 'archive.gz');
+
+const gzip = createGzip();
+const src = fs.createReadStream(pathToSrc);
+const destination = fs.createWriteStream(pathToDestination);
+
 export const compress = async () => {
-    // Write your code here 
+    pipeline(
+        src,
+        gzip,
+        destination,
+        err => {
+            if(err) {
+                console.error(err);
+                process.exitCode = 1;
+            }
+        }
+    )
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToSrc = path.join(__dirname, 'files','archive.gz' );
+const pathToDestination = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+const unzip = createUnzip();
+const src = fs.createReadStream(pathToSrc);
+const destination = fs.createWriteStream(pathToDestination);
 export const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        src,
+        unzip,
+        destination,
+        err => {
+            if(err) {
+                console.error(err);
+            }
+        }
+    )
 };
+
+decompress();
