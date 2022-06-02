032158c325051970a4aed3923a7d9018ac9e50f2
Elem <elem15@yandex.ru>
2022-06-02 11:54:56 +0300

    feat: solve hash tasks from assignment.md

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'fs/promises';
+const { createHmac } = await import('crypto');
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const data = await readFile(pathToFile, 'utf-8');
+    const hash = createHmac('sha256', 'password')
+               .update(data)
+               .digest('hex');
+    return hash;
 };
+
+console.log(await calculateHash());
